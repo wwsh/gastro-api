ec5ac245b89681d0a5acdcc93dd0f2e61341babc
Thomas Parys <thomas.parys@wwsh.io>
2016-03-16 17:12:47 +0100

    removing FOS UserBundle

--- src/WW/Gastro/ApiBundle/Entity/User.php
+++ src/WW/Gastro/ApiBundle/Entity/User.php
@@ -20,14 +20,13 @@
 
 namespace WW\Gastro\ApiBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-class User extends BaseUser
+class User
 {
     /**
      * @ORM\Id
